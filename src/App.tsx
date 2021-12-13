700de79fd2c8243953f390bdf77f0f2e2178e596
mluo24 <miranda.luo24@gmail.com>
2021-12-12 20:35:15 -0500

    added basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Container, Typography } from '@mui/material';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Container component="main" sx={{ mt: 4, mb: 2 }} maxWidth="md">
+        <Typography variant="h3" component="h1" gutterBottom>
+          Welcome!
+        </Typography>
+        <Typography variant="body1">
+          This is Sharefolio!
+        </Typography>
+      </Container>
+      <Footer />
+    </Box>
   );
 }
 
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,33 @@
+import { Box, Container, Link, Typography } from "@mui/material";
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary">
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Sharefolio
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const Footer = () => {
+    return (
+        <Box
+            component="footer"
+            sx={{
+                py: 4,
+                px: 2,
+                mt: 'auto'
+            }}
+        >
+            <Container maxWidth="md">
+                <Copyright />
+            </Container>
+        </Box>
+    )
+}
+
+export default Footer;
